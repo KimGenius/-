5fc499d8069b3e484352385b50e073fc56a6e747
half-but <qudcks9191@naver.com>
2017-11-04 22:54:18 +0900

    add firebase service

--- app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
+++ app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
@@ -2,6 +2,8 @@ package rbdd.highton_android.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 import rbdd.highton_android.Manager.GoogleSignInManager
 import rbdd.highton_android.R
@@ -14,6 +16,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("xxx", "" + FirebaseInstanceId.getInstance().token)
 
         googleSign.setOnClickListener {
             googleSignIn = GoogleSignInManager(MainActivity@this)
