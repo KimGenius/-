7ca6f1163c25a4fef98320e23a9097ce3d627e34
half-but <qudcks9191@naver.com>
2017-11-05 01:34:16 +0900

    add mainviewpager

--- app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
+++ app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
@@ -2,9 +2,14 @@ package rbdd.highton_android.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.util.Log
 import com.facebook.CallbackManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_content_main.*
+import rbdd.highton_android.Fragment.MainFragement
 import rbdd.highton_android.Manager.GoogleSignInManager
 import rbdd.highton_android.R
 import rbdd.highton_android.Util.BaseActivity
@@ -21,29 +26,20 @@ class MainActivity : BaseActivity() {
         GlideUtil.setGliding(this, R.drawable.ic_nav_drawer, navigationBarImg)
         GlideUtil.setGliding(this, R.drawable.ic_home_on, homeBtn)
         GlideUtil.setGliding(this, R.drawable.ic_list_off, listBtn)
-//        googleSign.setOnClickListener {
-//            googleSignIn = GoogleSignInManager(MainActivity@ this)
-//        }
-//
-//        callbackManager = CallbackManager.Factory.create()
-////        loginButton.setReadPermissions("email", "public_profile", "user_friends")
-//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
-//        loginButton.setReadPermissions("email")
-//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-//            override fun onSuccess(result: LoginResult?) {
-//                Log.d("facebook", result.toString())
-//                Log.d("facebook", result!!.accessToken.token)
-//            }
-//
-//            override fun onError(error: FacebookException?) {
-//                Log.d("facebookError", error.toString())
-//            }
-//
-//            override fun onCancel() {
-//                showToast("취소;")
-//            }
-//        })
 
+        viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+
+    }
+
+    class MainPagerAdapter(val fm: FragmentManager): FragmentStatePagerAdapter(fm){
+
+        override fun getItem(position: Int): Fragment {
+            return MainFragement()
+        }
+
+        override fun getCount(): Int {
+            return 10
+        }
 
     }
 
--- app/src/main/java/rbdd/highton_android/Fragment/MainFragement.kt
+++ app/src/main/java/rbdd/highton_android/Fragment/MainFragement.kt
@@ -1,7 +1,7 @@
 package rbdd.highton_android.Fragment
 
-import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -15,13 +15,14 @@ import rbdd.highton_android.R
  */
 class MainFragement: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_main, container, false)
         with(view){
-            recyclerView.layoutManager = LinearLayoutManager(container.context)
+            recyclerView.layoutManager = LinearLayoutManager(container?.context)
             recyclerView.adapter = MainAdapter()
         }
 
         return view
     }
+
 }
