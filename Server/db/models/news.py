b2007f9e483044c55073fafe1e98c7ced91d15d4
PlanB <city7310@naver.com>
2017-11-05 02:23:08 +0900

    [UPDATE] StringFIeld to DateTimeField / RefernceField, Declare Notification EmbeddedDocument

--- Server/db/models/news.py
+++ Server/db/models/news.py
@@ -1,11 +1,12 @@
 from bson.objectid import ObjectId
 
 from db.mongo import *
+from db.models.user import AccountModel
 
 
 class Comment(EmbeddedDocument):
     id = ObjectIdField(primary_key=True, default=ObjectId())
-    writer = StringField(required=True)
+    writer = ReferenceField(AccountModel, required=True)
     content = StringField(required=True)
     like_count = IntField(required=True, default=0)
 
@@ -14,7 +15,7 @@ class NewsModel(Document):
     title = StringField(required=True)
     description = StringField(required=True)
     link = StringField(required=True)
-    pub_date = StringField(required=True)
+    pub_date = DateTimeField(required=True)
     like_count = IntField(required=True, default=0)
     unlike_count = IntField(required=True, default=0)
 
--- Server/db/models/user.py
+++ Server/db/models/user.py
@@ -1,6 +1,11 @@
-from datetime import date
+from datetime import date, datetime
 
 from db.mongo import *
+
+
+class Notification(EmbeddedDocument):
+    content = StringField(required=True)
+    notification_time = StringField(str(datetime.now())[:-7])
 
 
 class AccountModel(Document):
@@ -9,6 +14,7 @@ class AccountModel(Document):
     email = StringField(unique=True)
     name = StringField(required=True, default='익명 사용자')
 
+    notification_queue = ListField(EmbeddedDocumentField(Notification), default=[])
     signup_date = StringField(required=True, default=str(date.today()))
     contribution_score = IntField(required=True, default=0)
     comment_count = IntField(required=True, default=0)
