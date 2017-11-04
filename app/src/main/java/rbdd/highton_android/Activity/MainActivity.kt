2b0a801082dcd76d4f7b494dad2ad7c896307125
Kim0Jae <youngjae1047@naver.com>
2017-11-05 01:33:38 +0900

    add facebookmanager

--- app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
+++ app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
@@ -21,30 +21,6 @@ class MainActivity : BaseActivity() {
         GlideUtil.setGliding(this, R.drawable.ic_nav_drawer, navigationBarImg)
         GlideUtil.setGliding(this, R.drawable.ic_home_on, homeBtn)
         GlideUtil.setGliding(this, R.drawable.ic_list_off, listBtn)
-//        googleSign.setOnClickListener {
-//            googleSignIn = GoogleSignInManager(MainActivity@ this)
-//        }
-//
-//        callbackManager = CallbackManager.Factory.create()
-////        loginButton.setReadPermissions("email", "public_profile", "user_friends")
-//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"))
-//        loginButton.setReadPermissions("email")
-//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-//            override fun onSuccess(result: LoginResult?) {
-//                Log.d("facebook", result.toString())
-//                Log.d("facebook", result!!.accessToken.token)
-//            }
-//
-//            override fun onError(error: FacebookException?) {
-//                Log.d("facebookError", error.toString())
-//            }
-//
-//            override fun onCancel() {
-//                showToast("취소;")
-//            }
-//        })
-
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
---
+++ app/src/main/java/rbdd/highton_android/Manager/FacebookSignInManager.kt
@@ -0,0 +1,37 @@
+package rbdd.highton_android.Manager
+
+import android.util.Log
+import android.view.View
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import rbdd.highton_android.Util.BaseActivity
+import java.util.*
+
+/**
+ * Created by young on 2017-11-05/오전 1:24
+ * This Project is HighThon-Trump
+ */
+class FacebookSignInManager {
+    fun facebookSignIn(activity: BaseActivity, loginButton: LoginButton, callbackManager: CallbackManager) {
+        LoginManager.getInstance().logInWithReadPermissions(activity, Arrays.asList("public_profile"))
+        loginButton.setReadPermissions("email")
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                Log.d("facebook", result.toString())
+                Log.d("facebook", result!!.accessToken.token)
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.d("facebookError", error.toString())
+            }
+
+            override fun onCancel() {
+
+            }
+        })
+    }
+}
