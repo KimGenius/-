31082fa4ad5c7ff9d9b6895e4e2e34671d597510
half-but <qudcks9191@naver.com>
2017-11-04 21:07:09 +0900

    add Activity pakage

--- app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
+++ app/src/main/java/rbdd/highton_android/Activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package rbdd.highton_android
+package rbdd.highton_android.Activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import rbdd.highton_android.R
 
 class MainActivity : AppCompatActivity() {
 
