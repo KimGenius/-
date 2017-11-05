a31f375760334cc7874586a2bbb0f1e2cbb607bd
Kim0Jae <youngjae1047@naver.com>
2017-11-05 09:33:53 +0900

    add Splash

---
+++ app/src/main/java/rbdd/highton_android/Activity/SplashActivity.kt
@@ -0,0 +1,24 @@
+package rbdd.highton_android.Activity
+
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash.*
+import rbdd.highton_android.R
+import rbdd.highton_android.Util.BaseActivity
+import rbdd.highton_android.Util.GlideUtil
+
+/**
+ * Created by young on 2017-11-05/오전 9:23
+ * This Project is HighThon-Trump
+ */
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        GlideUtil.setGliding(this@SplashActivity, R.drawable.splash, splashImg)
+
+        Handler().postDelayed({
+            goNextActivity(LoginActivity::class.java, true)
+        }, 1000)
+    }
+}
